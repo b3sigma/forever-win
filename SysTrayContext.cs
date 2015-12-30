cbbb3395a02c437e99fe23ce2f3d772891a3d563
b3sigma <b3sigma@gmail.com>
2015-12-29 21:58:09 -0800

    Ignored .suo from git. Whitespace/formatting only change to .cs files using batch reformat.

--- SysTrayContext.cs
+++ SysTrayContext.cs
@@ -6,58 +6,58 @@ using System.Drawing;
 
 namespace forever
 {
-    class SysTrayContext: ApplicationContext
+  class SysTrayContext : ApplicationContext
+  {
+    private System.ComponentModel.IContainer container;
+    private NotifyIcon notifyIcon;
+    private ContextMenuStrip contextMenu;
+    private ToolStripMenuItem menuItemExit;
+    private Timer timer;
+
+    public SysTrayContext()
     {
-        private System.ComponentModel.IContainer container;
-        private NotifyIcon notifyIcon;
-        private ContextMenuStrip contextMenu;
-        private ToolStripMenuItem menuItemExit;
-        private Timer timer;
+      container = new System.ComponentModel.Container();
 
-        public SysTrayContext()
-        {
-            container = new System.ComponentModel.Container();
-    
-            notifyIcon = new NotifyIcon(this.container);
-            notifyIcon.Icon = forever.icon;
+      notifyIcon = new NotifyIcon(this.container);
+      notifyIcon.Icon = forever.icon;
 
-            notifyIcon.Text = "Forever";
-            notifyIcon.Visible = true;
+      notifyIcon.Text = "Forever";
+      notifyIcon.Visible = true;
 
-            //Instantiate the context menu and items
-            contextMenu = new ContextMenuStrip();
-            menuItemExit = new ToolStripMenuItem();
+      //Instantiate the context menu and items
+      contextMenu = new ContextMenuStrip();
+      menuItemExit = new ToolStripMenuItem();
 
-            //Attach the menu to the notify icon
-            notifyIcon.ContextMenuStrip = contextMenu;
+      //Attach the menu to the notify icon
+      notifyIcon.ContextMenuStrip = contextMenu;
 
-            menuItemExit.Text = "Exit";
-            menuItemExit.Click += new EventHandler(menuItemExit_Click);
-            contextMenu.Items.Add(menuItemExit);
+      menuItemExit.Text = "Exit";
+      menuItemExit.Click += new EventHandler(menuItemExit_Click);
+      contextMenu.Items.Add(menuItemExit);
 
-            timer = new Timer(this.container);
-            timer.Interval = 1000;
-            timer.Tick += new EventHandler(timer_Tick);
-            timer.Start();
-        }
-
-        void timer_Tick(object sender, EventArgs e)
-        {
-            if (!Program.WatchersRunning) ExitThreadCore();
-        }
-
-        void menuItemExit_Click(object sender, EventArgs e)
-        {
-            ExitThreadCore();
-        }
-
-        protected override void ExitThreadCore()
-        {
-            Program.stop = true;
-
-            notifyIcon.Visible = false;
-
-            base.ExitThreadCore();
-        }
+      timer = new Timer(this.container);
+      timer.Interval = 1000;
+      timer.Tick += new EventHandler(timer_Tick);
+      timer.Start();
     }
+
+    void timer_Tick(object sender, EventArgs e)
+    {
+      if (!Program.WatchersRunning) ExitThreadCore();
+    }
+
+    void menuItemExit_Click(object sender, EventArgs e)
+    {
+      ExitThreadCore();
+    }
+
+    protected override void ExitThreadCore()
+    {
+      Program.stop = true;
+
+      notifyIcon.Visible = false;
+
+      base.ExitThreadCore();
+    }
+  }
 }
