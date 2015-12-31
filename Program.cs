ce40c168c065df3b297f3e5dd8719b17b8edb6c3
b3sigma <b3sigma@gmail.com>
2015-12-30 18:10:32 -0800

    Checking in broken state... In process adding some simple file watcher system to restart.

--- Program.cs
+++ Program.cs
@@ -13,26 +13,69 @@ namespace forever {
     public static volatile bool stop = false;
 
     public static Thread[] runningWatchers;
+    public static string[] runningProcessNames;
+    public static string _watchFileName;
+    public static int _watchFileSeconds = 20;
+    public static float lastWatchTime = 0.0f;
 
     public static bool WatchersRunning {
       get {
-        foreach(Thread t in runningWatchers)
-          if(t.IsAlive)
+        foreach(Thread t in runningWatchers) {
+          if(t.IsAlive) {
             return true;
+          }
+        }
         return false;
       }
     }
 
     static void Main(string[] args) {
-      if(args.Length == 0 || (args.Length & 1) == 1) {
+      //if(args.Length == 0 || (args.Length & 1) == 1) {
+      if(args.Length <= 1) {
         Usage();
         return;
       }
 
-      runningWatchers = new Thread[args.Length / 2];
+      // yeah this is lame.
+      // miss single file header style libraries,
+      // too lazy to figure out how to do chained library for c#
+      // will regret probably
+      int watchFileDelay = 60;
+      int startProgramList = args.Length;
+      for(int pArg = 0; pArg < args.Length; pArg++) {
+        
+        if(!args[pArg].StartsWith("--")) {
+          startProgramList = pArg;
+          break;
+        } else if(args[pArg].StartsWith("--watch_file", StringComparison.CurrentCultureIgnoreCase)) {
+          _watchFileName = args[pArg].Substring("--watch_file=".Length);
+        } else if(args[pArg].StartsWith("--watch_file_seconds", StringComparison.CurrentCultureIgnoreCase)) {
+          string numSeconds = args[pArg].Substring("--watch_file_seconds=".Length);
+          _watchFileSeconds = Int32.Parse(numSeconds);
+        } else if(args[pArg].StartsWith("--watch_file_delay", StringComparison.CurrentCultureIgnoreCase)) {
+          string numSeconds = args[pArg].Substring("--watch_file_delay=".Length);
+          watchFileDelay = Int32.Parse(numSeconds);
+        }
+      }
 
-      for(int i = 0; i < args.Length / 2; i++) {
-        StartAndWatch(args[i * 2], args[i * 2 + 1], ref runningWatchers[i]);
+      int numPrograms = (args.Length - startProgramList) / 2;
+
+      if(numPrograms <= 0) {
+        Usage();
+        return;
+      }
+
+      runningWatchers = new Thread[numPrograms];
+      runningProcessNames = new string[numPrograms];
+
+      for(int p = 0; p < numPrograms; p++) {
+        int programIndex = startProgramList + (p * 2);
+        runningProcessNames[p] = args[programIndex]; 
+        StartAndWatch(args[programIndex], args[programIndex + 1], ref runningWatchers[p]);
+      }
+
+      if(_watchFileName.Length > 2) {
+        StartFileWatch(watchFileDelay);
       }
 
       Application.EnableVisualStyles();
@@ -42,9 +85,53 @@ namespace forever {
 
     }
 
+    static void StartFileWatch(int startDelay) {
+       MessageBox.Show(
+        "Would have monitored [" + _watchFileName + "] with time + ["
+            + _watchFileSeconds + "] starting after [" + startDelay + "]" ,
+        "debuggo",
+        MessageBoxButtons.OK,
+        MessageBoxIcon.Information,
+        MessageBoxDefaultButton.Button1
+      );
+      return;
+
+      Thread thread = new Thread(delegate() {
+
+        DateTime lastTime = System.IO.File.GetLastWriteTime(_watchFileName);
+
+        Thread.Sleep(startDelay * 1000);
+
+        while(true) {
+
+          DateTime thisTime = System.IO.File.GetLastWriteTime(_watchFileName);
+
+          // if it hasn't changed in _watchFileSeconds (roughly since we last checked), assume it crashed
+          if(thisTime.Subtract(lastTime).Seconds > _watchFileSeconds) {
+            foreach (string processName in runningProcessNames) {
+              Process [] localByName = Process.GetProcessesByName(processName);
+              foreach(Process p in localByName)
+              {
+                 p.Kill();
+              }
+            }
+
+            Thread.Sleep(startDelay * 1000);
+          } else {
+            Thread.Sleep(_watchFileSeconds * 1000);
+          }
+        }
+      });
+      thread.Start();
+    }
+    
     static void Usage() {
       MessageBox.Show(
-          "usage:\n\nforever <application1> \"<arguments1>\" [<application2> \"<arguments2>\"] ..",
+          "usage:\n\nforever \n"
+              + "--watchFile=<filename>\n"
+              + "--watchFileSeconds=<seconds>\n"
+              + "--watchFileDelay=<seconds>\n"
+              + "<application1> \"<arguments1>\" [<application2> \"<arguments2>\"] ..",
           "forever usage",
           MessageBoxButtons.OK,
           MessageBoxIcon.Information,
@@ -52,7 +139,17 @@ namespace forever {
       );
     }
 
-    static void StartAndWatch(string fileName, string fileArgs, ref Thread thread) {
+    static void StartAndWatch(
+        string fileName, string fileArgs, ref Thread thread) {
+      MessageBox.Show(
+        "Would have launched [" + fileName + "] with args + [" + fileArgs + "]",
+        "debuggo",
+        MessageBoxButtons.OK,
+        MessageBoxIcon.Information,
+        MessageBoxDefaultButton.Button1
+      );
+      return;
+
       thread = new Thread(delegate() {
         var psi = new ProcessStartInfo {
           FileName = fileName,
